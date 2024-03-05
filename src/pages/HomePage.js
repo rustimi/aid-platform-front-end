0227e761d33730c561887b810ba8ed9bc7a7b4e6
rustimi <michele.rusticucci@gmail.com>
2024-03-05 23:55:01 +0100

    IMP homepage with bootstrap

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,33 @@
+import { Link } from "react-router-dom";
+
 export default function HomePage() {
     return (
-      <div>
-        <h1>Home Page</h1>
+      <div className="homepage container-fluid p-0">
+        <div className="logo-img m-sm-auto"></div>
+        <div className="row g-0">
+          <div className="col-12 col-lg-7 big-block bg-primary d-flex align-items-center justify-content-center">
+            <div className="text-white">
+              <h1>Kind Quest - community help</h1>
+              <p>Be a part of power of the community</p>
+              <div className="row">
+                <div className="col -6">
+                <Link to="/signup" className="btn btn-secondary w-100">Sign Up</Link>
+                </div>
+                <div className="col-6">
+                <Link to="/login" className="btn btn-secondary w-100">Login</Link>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-12 col-lg-5 big-block bg-image">
+          </div>
+        </div>
+        <div className="row d-flex align-items-center big-block bg-secondary" >
+          <div className="col-lg-8 col-10 m-auto text-center text-white">
+            <h1>Be the hero your neighborhood needs</h1>
+            <p>This platform is your doorway to making a difference, where every act of kindness is a step towards a more compassionate world. Whether you're offering support or seeking it, you're never alone. With features designed to make volunteering seamless and meaningful, from direct messaging to real-time updates on unfulfilled requests, your goodwill finds its way exactly where it's needed. As you explore our homepage on</p>
+          </div>
+        </div>
       </div>
     );
   }
