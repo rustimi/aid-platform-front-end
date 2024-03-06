d9be995dd40cceab18da6afc93c4c7dbc7560731
rustimi <michele.rusticucci@gmail.com>
2024-03-06 23:40:23 +0100

    IMP back to homepage navigation

--- src/pages/SignupPage.js
+++ src/pages/SignupPage.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../components/AuthContext';
 import { API_BASE_URL } from '../components/config';
 import axios from 'axios';
@@ -38,7 +38,14 @@ export default function SignupPage() {
 
     return (
         <>
-            <div className="logo-img m-sm-auto"></div>
+            <div className="logo-img m-sm-auto d-flex justify-content-center">
+                <Link to="/" className="position-absolute bottom-0 btn btn-sm text-dark">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
+                        <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
+                    </svg>
+                    Homepage
+                </Link>
+            </div>
             <div className="min-vh-100 d-flex justify-content-center align-items-center bg-secondary">
                 <form onSubmit={handleSubmit} className="p-4 bg-white col-11 col-lg-2" style={{ borderRadius: '5px' }}>
                     <h1 className="text-center text-primary">Kind Quest</h1>
