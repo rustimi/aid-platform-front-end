f5ff3f32eecd957d6e525bcbae3740233c35bea9
rustimi <michele.rusticucci@gmail.com>
2024-03-06 23:08:52 +0100

    Made login prettier

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -14,19 +14,31 @@ export default function LoginPage() {
     try {
       await login(email, password);
       // Redirect to another page on successful login
-      navigate('/dashboard'); 
+      navigate('/dashboard');
     } catch (error) {
       // Handle login error (e.g., show an error message)
       console.error('Login failed:', error);
     }
   };
-  
+
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-      <button type="submit">Login</button>
-    </form>
+    <>
+      <div className="logo-img m-sm-auto"></div>
+      <div className="min-vh-100 d-flex justify-content-center align-items-center bg-secondary">
+        <form onSubmit={handleSubmit} className="p-4 bg-white col-11 col-lg-2" style={{ borderRadius: '5px' }}>
+        <h1 className="text-center text-primary">Kind Quest</h1>
+          <div className="mb-3">
+            <input type="email" className="form-control" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email address" />
+          </div>
+          <div className="mb-3">
+            <input type="password" className="form-control" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+          </div>
+          <button type="submit" className="btn btn-primary w-100">Login</button>
+        </form>
+      </div>
+
+    </>
+
   );
 }
