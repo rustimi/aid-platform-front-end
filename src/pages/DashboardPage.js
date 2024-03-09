120b65eaf46b4457de049d8e41065f0039a64589
rustimi <michele.rusticucci@gmail.com>
2024-03-09 16:45:56 +0100

    IMP infowindow

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
+import React, { useState } from 'react'
+import { GoogleMap, useJsApiLoader, Marker, InfoWindow } from '@react-google-maps/api';
 import { debounce } from 'lodash';
 import CardComponent from '../components/card';
 
@@ -19,7 +19,8 @@ export default function DashboardPage() {
         googleMapsApiKey: "AIzaSyDcBfcLxJIXSvH2fZmYHrSTqS8w2E3-ywo"
     })
 
-    const [map, setMap] = React.useState(null)
+    const [map, setMap] = useState(null)
+    const [showInfoWindow, setShowInfoWindow] = useState(false);
 
     const onLoad = React.useCallback(function callback(map) {
         // This is just an example of getting and using the map instance!!! don't just blindly copy!
@@ -35,12 +36,23 @@ export default function DashboardPage() {
 
     // debounce the function so it only fires after the user stops moving the map
     const fetchRequestsBasedOnBounds = React.useCallback(debounce(() => {
+        setShowInfoWindow(false)
         if (map) {
             const bounds = map.getBounds();
             console.log('Fetching something based on these bounds:', bounds.toString());
             // Your API call logic here
         }
     }, 800), [map]);
+
+    const position = {
+        lat: -3.745,
+        lng: -38.523
+    }
+
+    const onMarkerClick = () => {
+        setShowInfoWindow(true);
+    };
+
 
     return isLoaded ? (
         <div className="container-fluid bg-dark p-0 m-0 row big-block   ">
@@ -63,8 +75,23 @@ export default function DashboardPage() {
                     onUnmount={onUnmount}
                     onBoundsChanged={fetchRequestsBasedOnBounds}
                 >
-                    { /* Child components, such as markers, info windows, etc. */}
-                    <></>
+                    <Marker
+                        position={position}
+                        onClick={onMarkerClick} // Set the click handler
+                    />
+
+                    {showInfoWindow && (
+                        <InfoWindow
+                            position={position}
+                            onCloseClick={() => setShowInfoWindow(false)} // Close the InfoWindow when its close button is clicked
+                        >
+                            <div>
+                                <h3>Marker Info</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam eu turpis hendrerit, placerat ipsum at, vulputate purus. Fusce euismod ut mauris in suscipit. Nam varius neque a magna fringilla finibus nec et neque. Curabitur vel egestas est, vitae gravida eros. Quisque semper mattis ex, id malesuada</p>
+                            </div>
+                        </InfoWindow>
+                    )}
+
                 </GoogleMap>
             </div>
         </div>
