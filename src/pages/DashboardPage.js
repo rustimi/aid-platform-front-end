c1e9f0bec4a0016fd9520430b3facb3fb57a3402
rustimi <michele.rusticucci@gmail.com>
2024-03-09 20:34:50 +0100

    IMP fill cards based on bounds search from API

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 import { debounce } from 'lodash';
+import axios from 'axios';
+import { API_BASE_URL } from '../components/config';
 import CardComponent from '../components/card';
 import MarkerComponent from '../components/marker';
 
@@ -17,7 +19,8 @@ export default function DashboardPage() {
 
     const [map, setMap] = useState(null)
     const [activeId, setActiveId] = useState(null);
-    const [mapCenter, setMapCenter] = useState({lat: 0, lng: 0}); 
+    const [mapCenter, setMapCenter] = useState({ lat: 0, lng: 0 });
+    const [requests, setRequests] = useState([]);
 
     useEffect(() => { // Get the user's location and set the map center to it
         if ("geolocation" in navigator) {
@@ -29,13 +32,10 @@ export default function DashboardPage() {
                     });
                 }
             );
-        } 
+        }
     }, []);
 
     const onLoad = React.useCallback(function callback(map) {
-        // const bounds = new window.google.maps.LatLngBounds(mapCenter);
-        // map.fitBounds(bounds);
-
         setMap(map)
     }, [])
 
@@ -45,30 +45,34 @@ export default function DashboardPage() {
 
     // debounce the function so it only fires after the user stops moving the map
     const fetchRequestsBasedOnBounds = React.useCallback(debounce(() => {
-        if (map) {
+        if (map && map.getBounds()) {
             const bounds = map.getBounds();
-            console.log('Fetching something based on these bounds:', bounds.toString());
-            // Your API call logic here
+            const ne = bounds.getNorthEast();
+            const sw = bounds.getSouthWest();
+            const query = `ne_latitude=${encodeURIComponent(ne.lat())}&ne_longitude=${encodeURIComponent(ne.lng())}&sw_latitude=${encodeURIComponent(sw.lat())}&sw_longitude=${encodeURIComponent(sw.lng())}`;
+            console.log(query)
+            axios.get(`${API_BASE_URL}/requests?${query}`)
+                .then(response => {
+                    setRequests(response.data.requests);
+                }).catch(error => {
+                    console.error('Failed to fetch user info:', error);
+                });
         }
     }, 800), [map]);
 
     return isLoaded ? (
         <div className="container-fluid bg-dark p-0 m-0 row big-block   ">
             <div className='col-4 d-flex flex-column pt-2'>
-                <CardComponent
-                    id={1}
-                    activeId={activeId}
-                    type='material need'
-                    title='This is the body of the card'
-                    setActiveId={setActiveId}
-                />
-                <CardComponent
-                    id={2}
-                    activeId={activeId}
-                    type='one time task'
-                    title='This is the body of the card'
-                    setActiveId={setActiveId}
-                />
+                {requests.map((request) => ( // Step 3: Map over requests to render CardComponent
+                    <CardComponent
+                        key={request.id}
+                        id={request.id}
+                        activeId={activeId}
+                        type={request.request_type}
+                        title={request.description}
+                        setActiveId={setActiveId}
+                    />
+                ))}
             </div>
             <div className='col-8 p-2'>
                 <GoogleMap
