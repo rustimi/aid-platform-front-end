5614f6f60b76adb0b039b855b42fd3e74b3691d8
rustimi <michele.rusticucci@gmail.com>
2024-03-09 23:45:18 +0100

    REF button name to fulfill request

--- src/components/card.js
+++ src/components/card.js
@@ -10,7 +10,7 @@ export default function CardComponent({ id, activeId, type, title, setActiveId }
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
                 {activeId === id && ( // Corrected: Use logical AND for conditional rendering
-                    <Link to={`/requests/${id}`} className="btn btn-primary float-end">View</Link>
+                    <Link to={`/requests/${id}`} className="btn btn-primary float-end">Fulfill</Link>
                 )}
             </div>
         </div>)
