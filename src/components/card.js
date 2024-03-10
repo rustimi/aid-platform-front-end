eb33829bfc83c9f9d3879d22a8570092bc4a5a92
rustimi <michele.rusticucci@gmail.com>
2024-03-10 10:38:36 +0100

    IMP button to requests page and minora color changes

--- src/components/card.js
+++ src/components/card.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 export default function CardComponent({ id, activeId, type, title, setActiveId }) {
     const isActiveClass = activeId === id ? 'bg-selected' : '';
-    const bg_color = type.toLowerCase() === 'material need' ? 'bg-secondary' : 'bg-primary';
+    const bg_color = type.toLowerCase() === 'material need' ? 'bg-secondary' : 'bg-custom';
 
     return (
         <div className={`request card text-white mb-3 ${bg_color} ${isActiveClass}`} onClick={() => setActiveId(id)}>
