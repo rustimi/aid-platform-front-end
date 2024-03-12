356cad4318d9729048bf691844e3fc45f6d2da91
rustimi <michele.rusticucci@gmail.com>
2024-03-12 22:40:41 +0100

    IMP new message

--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -7,48 +7,39 @@ import MessageComponent from '../components/message';
 export default function ChatPage() {
     const { id, conversationId } = useParams();
     const [messageBody, setMessageBody] = useState('');
+    const [requestTitle, setRequestTitle] = useState('');
     const [messages, setMessages] = useState([]);
-    const [newMessageStatus, setNewMessageStatus] = useState(null);
-    const [newMessageErrors, setNewMessageErrors] = useState({});
     const [showError, setShowError] = useState(false);
 
     useEffect(() => { // Load conversations
+        loadMessages()
+    }, []);
+
+    const loadMessages = () => {
         setShowError(false);
-        setMessages([]);
         axios.get(`${API_BASE_URL}/requests/${id}/conversations/${conversationId}/messages`)
             .then(response => {
                 setMessages(response.data.messages)
+                setRequestTitle(response.data.request_name)
             }).catch(error => {
                 setShowError(true);
             });
-    }, []);
-
-
-    const handleSubmitNewMessage = async (e) => {
+    }
+    const handleSubmitNewMessage = async (e) => { // Send new message
         e.preventDefault();
-        axios.put(`${API_BASE_URL}/requests`, { // Create request
-            // title: title,
-            // description: description,
-            // request_type: type,
-            // latitude: coordinates.lat,
-            // longitude: coordinates.lng
+        setShowError(false)
+        axios.put(`${API_BASE_URL}/requests/${id}/conversations/${conversationId}/messages`, {
+            body: messageBody,
         })
             .then(response => {
                 if (response.status === 201) {
-                    setNewMessageStatus(true)
+                    loadMessages()
+                    setMessageBody('')
                 }
             })
             .catch(errors => {
                 console.log(errors)
-                let objNewMessageErrors = {};
-
-                // Format error messages
-                errors.response.data.forEach(error => {
-                    // let split = error.split(" ") // Error message format: "Email has invalid format"
-                    // objUserCreateErrors[split[0].toLowerCase()] = split.slice(1).join(" ")
-                })
-                setNewMessageErrors(objNewMessageErrors);
-                setNewMessageStatus(false)
+                setShowError(true)
             })
     };
 
@@ -61,7 +52,7 @@ export default function ChatPage() {
                             <Link to={`/requests/${id}/conversations`} className="btn btn-warning btn-sm float-start ">Back to conversations</Link>
                         </div>
                         <div className="chat-page pb-2 pt-2 bg-white col-11 col-lg-8" style={{ borderRadius: '5px' }}>
-                            <h1 className=" text-primary m-3">NOME - request title</h1>
+                            <h1 className=" text-primary m-3 border-bottom  ">{requestTitle}</h1>
                             <div className="conversation-container d-flex flex-column">
                                 {messages.map((message) => {
                                     return (
@@ -79,11 +70,10 @@ export default function ChatPage() {
                                 <form onSubmit={handleSubmitNewMessage} className='d-flex p-2' >
                                     <textarea type="text" className="form-control" value={messageBody} onChange={(e) => setMessageBody(e.target.value)} placeholder="New message" />
                                     <button type="submit" className={`btn btn-dark align-self-end m-3 mb-0`}>Send</button>
-                                    {newMessageErrors.title && <div className="pl-3 text-danger">{newMessageStatus.messageBody}</div>}
-                                    <div className={`alert alert-success mt-2 ${newMessageStatus === true ? '' : 'd-none'}`} role="alert">
-                                        Create successful! :)
-                                    </div>
                                 </form>
+                            </div>
+                            <div className={`alert alert-danger mt-2 ${showError === true ? '' : 'd-none'}`} role="alert">
+                                There has been an error. Try later
                             </div>
                         </div>
                     </div>
