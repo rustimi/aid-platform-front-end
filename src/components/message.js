f62455f77d41310ac9f54511e525732f19bab301
rustimi <michele.rusticucci@gmail.com>
2024-03-12 22:20:27 +0100

    IMP chat component and fetch from api

--- src/components/message.js
+++ src/components/message.js
@@ -1,6 +1,5 @@
 import React from 'react';
 export default function MessageComponent({ body, isSent, isSeen, time }) {
-
     return (
         <div className={`message p-2 rounded bg-primary-subtle mb-2 w-75 ${isSent ? 'align-self-end':''}`}>
             <div className='message-content'>{body}</div>
--- src/pages/ChatPage.js
+++ src/pages/ChatPage.js
@@ -1,13 +1,28 @@
-import React, { useRef, useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import axios from 'axios';
 import { API_BASE_URL } from '../components/config';
 import MessageComponent from '../components/message';
 
 export default function ChatPage() {
+    const { id, conversationId } = useParams();
     const [messageBody, setMessageBody] = useState('');
+    const [messages, setMessages] = useState([]);
     const [newMessageStatus, setNewMessageStatus] = useState(null);
     const [newMessageErrors, setNewMessageErrors] = useState({});
+    const [showError, setShowError] = useState(false);
+
+    useEffect(() => { // Load conversations
+        setShowError(false);
+        setMessages([]);
+        axios.get(`${API_BASE_URL}/requests/${id}/conversations/${conversationId}/messages`)
+            .then(response => {
+                setMessages(response.data.messages)
+            }).catch(error => {
+                setShowError(true);
+            });
+    }, []);
+
 
     const handleSubmitNewMessage = async (e) => {
         e.preventDefault();
@@ -43,24 +58,22 @@ export default function ChatPage() {
                 <div className="row g-0">
                     <div className="col-12 big-block bg-primary d-flex flex-column align-items-center justify-content-center">
                         <div className='col-11 col-lg-8 mb-2'>
-                            <Link to="/requests" className="btn btn-warning btn-sm float-start ">Back</Link>
+                            <Link to={`/requests/${id}/conversations`} className="btn btn-warning btn-sm float-start ">Back to conversations</Link>
                         </div>
                         <div className="chat-page pb-2 pt-2 bg-white col-11 col-lg-8" style={{ borderRadius: '5px' }}>
                             <h1 className=" text-primary m-3">NOME - request title</h1>
                             <div className="conversation-container d-flex flex-column">
-                                <MessageComponent
-                                    body='Hello, how can I help you?'
-                                    time='12:00'
-                                    type='received'
-                                    isSent={false}
-                                    isSeen={true}
-                                />
-                                <MessageComponent
-                                    body='Hello, how can I help you in response'
-                                    time='12:00'
-                                    isSent={true}
-                                    isSeen={true}
-                                    />
+                                {messages.map((message) => {
+                                    return (
+                                        <MessageComponent
+                                            key={message.id}
+                                            body={message.body}
+                                            time={message.message_time}
+                                            isSent={message.isSent}
+                                            isSeen={message.read}
+                                        />
+                                    );
+                                })}
                             </div>
                             <div className='new-message-container'>
                                 <form onSubmit={handleSubmitNewMessage} className='d-flex p-2' >
