51d60903e595b8e38e8a6d6a1c2992190dbe14e2
rustimi <michele.rusticucci@gmail.com>
2024-03-12 20:26:08 +0100

    IMP Conversations page and component

--- src/components/conversation.js
+++ src/components/conversation.js
@@ -1,26 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default function RequestComponent({ id, type, title, description, isRepublishable, handleRepublishClick, handleFulfillClick }) {
-    const bg_color = type.toLowerCase() === 'material need' ? 'bg-secondary' : 'bg-custom';
+export default function RequestComponent({ id, sender, receiver }) {
 
     return (
-        <div className={`request card text-white mb-3 ${bg_color}`}>
-            <div className="card-header">
-                <p className='d-inline-block'>{type}</p>
-            </div>
-            <div className="card-body">
-                <h5 className="card-title">{title}</h5>
-                <p className='card-text'>{description}</p>
-                {(!isRepublishable &&
-                    <>
-                        <button onClick={()=>{handleFulfillClick(id)}} className="float-start btn btn-link text-warning">Mark as complete</button>
-                        <Link to={`${id}/conversations/`} className="btn btn-warning float-end shadow">Conversations</Link>
-                    </>
-                )}
-                {(isRepublishable &&
-                    <button onClick={()=>{handleRepublishClick(id)}} className="btn btn-warning float-end shadow">Republish</button>
-                )}
+        <div className={`request card text-white mb-3 bg-secondary`}>
+            <div className="card-body d-flex justify-content-between align-items-center">
+                <div>
+                    <h5 className="card-title">From: {sender.fname}, {sender.lname}</h5>
+                    <p className='card-text'>To: {receiver.fname}, {receiver.lname}</p>
+                </div>
+                <Link to={`${id}/chat`} className="btn btn-warning float-end shadow">Open Chat</Link>
             </div>
         </div>)
 }
--- src/pages/ConversationsPage.js
+++ src/pages/ConversationsPage.js
@@ -26,26 +26,28 @@ export default function ConversationsPage() {
     return (<>
         <div className="container-fluid conversations-container bg-primary-subtle p-0 m-0 justify-content-center row big-block   ">
             <div className='col-12 col-lg-11 bg-primary p-2 p-lg-5 pt-lg-0 mt-3 mb-3 shadow rounded'>
+            <div className='col-12 mt-2 mb-2'>
+                    <Link to="/requests" className="btn btn-warning btn-sm ">Back</Link>
+                </div>
+            <h1 className='text-light pb-2'>Active Conversations</h1>
                 <div className={`alert alert-danger mt-3 ${showError ? 'd-block' : 'd-none'}`}>
                     <strong>Oh snap!</strong> There has been an error. Please try again later!
                 </div>
-                <div className='conversations-navigation position-relative'>
-                    <div className="logo-img logo-img-sm d-flex justify-content-center">
-                        <Link to="/dashboard" className="position-absolute bottom-0 btn btn-sm text-light">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-bar-left" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M12.5 15a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5M10 8a.5.5 0 0 1-.5.5H3.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L3.707 7.5H9.5a.5.5 0 0 1 .5.5" />
-                            </svg>
-                            Dashboard
-                        </Link>
-                    </div>
-                </div>
-                <h1 className='text-light pb-2'>Active Conversations</h1>
-                
+                {conversations.map((conversation) => {
+                    return (
+                        <ConversationComponent
+                            key={conversation.id}
+                            id={conversation.id}
+                            sender={conversation.sender}
+                            receiver={conversation.receiver}
+                        />
+                    );
+                })}
                 <div className='mt-3 shadow'>
                     <div className={`alert alert-info ${conversations.length === 0 ? 'd-block' : 'd-none'}`}>
                         <strong>Oops, no conversations foud!</strong>
                     </div>
-                
+
                 </div>
             </div>
         </div>
