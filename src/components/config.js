5f4de40a14599483422f1d28720f0e94549eb505
rustimi <michele.rusticucci@gmail.com>
2024-03-13 22:33:28 +0100

    Revert "TEMP local jwt test"
    This reverts commit f9bebd807a62c73d57bb8309bb45e95f15915819.

--- src/components/config.js
+++ src/components/config.js
@@ -1,3 +1,2 @@
 // config.js
-// export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://aid-platform-api.onrender.com';
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'https://aid-platform-api.onrender.com';
